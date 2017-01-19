c05dcb84519d1b56192130026f494085b685fe0c
Krzysztof Biernat <kbiernat@fgtsa.com>
2017-01-19 17:40:07 +0100

    syslog history

---
+++ bash/syslog-history.sh
@@ -0,0 +1,16 @@
+# History to syslog
+PROMPT_COMMAND='AT_PROMPT=t'
+function log2syslog
+{
+    [ -z "${AT_PROMPT+set}" ] && return 1
+    COMMAND=$(fc -ln -0|cut -f 2-)
+    if [ -n "${BASH_COMMAND}" ] && [ "${BASH_COMMAND}" != "AT_PROMPT=t" ]; then
+        if [ "${USER}" != "$(logname)" ]; then
+            LOGUSER="$(logname)@${USER}"
+        fi
+        logger -p local4.notice -t bash -i -- "${LOGUSER:-$USER}: ${PWD} : ${COMMAND}"
+    fi
+    unset AT_PROMPT
+} 
+trap log2syslog DEBUG
+
